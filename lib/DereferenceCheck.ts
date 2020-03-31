18fc1e18553c908ccd5776ae25e40b20c1be7ca4
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-31 09:08:49 +0200

    Rewrote dereference check with fetch ap i

--- lib/DereferenceCheck.ts
+++ lib/DereferenceCheck.ts
@@ -1,11 +1,12 @@
 import {checkResult, IValidURI} from "./IValidURI";
 
+const fetch = require('node-fetch');
 
 
-export class DereferenceCheck implements IValidURI{
+export class DereferenceCheck implements IValidURI {
     private URI: string;
 
-    constructor(uri: string){
+    constructor(uri: string) {
         this.URI = uri;
     }
 
@@ -16,29 +17,26 @@ export class DereferenceCheck implements IValidURI{
     }
 
     private async checkRedirect() {
-        let protocol: any;
-        if(this.URI.split('://')[0] === 'https'){
-            protocol = require('https');
-        } else {
-            protocol = require('http');
-        }
 
         //let https = require('https');
         let idPos = this.URI.indexOf('/id/');
         let result;
 
-        if(idPos > 0){
+        if (idPos > 0) {
             result = await new Promise(resolve => {
                 let result;
-                protocol.get(this.URI, (res: any) => {
-                    const redirectURI = this.URI.split('://')[0] + '://' + res.connection.servername + res.headers.location;        // First part should be replaced
-                    const requestURI = this.URI.replace('/id/', '/doc/');
-
-                    if(res.statusCode === 303 && redirectURI === requestURI){
-                         result = {satisfied: true, message: 'Correct redirect from URI with type id to same URI but with type doc.'};
-                     } else {
-                         result = {satisfied: false, message: 'Incorrect. Or response code is not 303 or redirect URL is incorrect.'};
-                     }
+                fetch(this.URI).then((response: any) => {
+                    if (response.redirected === true && this.URI.replace('/id/', '/doc/') === response.url) {
+                        result = {
+                            satisfied: true,
+                            message: 'Correct redirect from URI with type id to same URI but with type doc.'
+                        };
+                    } else {
+                        result = {
+                            satisfied: false,
+                            message: 'Incorrect. Or response code is not 303 or redirect URL is incorrect.'
+                        };
+                    }
                     resolve(result);
                 });
             });
@@ -48,44 +46,38 @@ export class DereferenceCheck implements IValidURI{
         return result;
     }
 
-    private async checkSupportedSerializations(){
-        let protocol: any;
-        if(this.URI.split('://')[0] === 'https'){
-            // By default the http(s) modules does not follow redirects
-            protocol = require('follow-redirects').https;
-        } else {
-            protocol = require('follow-redirects').http;
-        }
+    private async checkSupportedSerializations() {
         let serializations = ['text/html', 'text/turtle', 'application/ld+json', 'application/rdf+xml', 'application/n-triples'];
-        let result: {[s: string] : object} = {};
+        let result: { [s: string]: object } = {};
 
-        for(let index in serializations){
+        for (let index in serializations) {
             const options = {
-                headers : {
+                headers: {
                     'Accept': serializations[index]
                 }
             }
 
             let serResult: object = await new Promise(resolve => {
-                protocol.get(this.URI, options, (res: any) => {
+                fetch(this.URI, options).then((response: any) => {
                     let result = {};
-                    let contentType = res.headers['content-type'].split(';')[0];
+                    const contentType = response.headers.get('content-type');
 
-                    if(res.statusCode === 200 && contentType == serializations[index]){
+                    if (response.status === 200 && contentType == serializations[index]) {
                         result = {satisfied: true, message: 'Format supported.'};
                     } else {
                         result = {satisfied: false, message: 'Format not supported'};
                     }
 
                     // Special cases -- application/ld+json returns application/json
-                    if(res.statusCode === 200 && serializations[index] == 'application/ld+json' && contentType == 'application/json'){
+                    if (response.status === 200 && serializations[index] === 'application/ld+json' && contentType == 'application/json') {
                         result = {satisfied: true, message: 'Format supported.'};
                     }
 
                     // application/n-triples returns text/ntriples
-                    if(res.statusCode === 200 && serializations[index] == 'application/n-triples' && contentType == 'text/ntriples'){
+                    if (response.status === 200 && serializations[index] === 'application/n-triples' && contentType == 'text/ntriples') {
                         result = {satisfied: true, message: 'Format supported.'};
                     }
+
                     resolve(result);
                 });
             });

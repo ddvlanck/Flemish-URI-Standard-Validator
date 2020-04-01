66b04f9679e6f8aa53ccf4c1e14fe4205d3f97d7
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-04-01 11:35:26 +0200

    Removed function for Typecheck

--- lib/TypeCheck.ts
+++ lib/TypeCheck.ts
@@ -14,19 +14,8 @@ export class TypeCheck implements IValidURI{
     }
 
     // Here we check if the URL contains id, doc or ns
-    checkURI(): TypeResult {
-        let idPos = this.URI.indexOf('/id/');
-        let docPos = this.URI.indexOf('/doc/');
-        let nsPos = this.URI.indexOf('/ns/');
-
-        let result: checkResult;
-
-        if(idPos < 0 && docPos < 0 && nsPos < 0){
-            result = {satisfied: false, message : "{type} is niet 'id', 'doc' of 'ns'."};
-        } else {
-            result = {satisfied: true, message : ""};
-        }
-
-        return {type: result};
+    checkURI(): object {
+        return {};
     }
+
 }
